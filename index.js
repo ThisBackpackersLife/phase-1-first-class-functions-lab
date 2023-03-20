19c7683105e865acbace886d207c2095ea328d60
Nicholas <crazedglitch@yahoo.com>
2023-03-20 08:59:14 -0500

    lab complete

--- index.js
+++ index.js
@@ -1,69 +1,21 @@
 // Code your solution in this file!
-const returnFirstTwoDrivers = function(drivers) {
-    return drivers.slice(0, 2);
-} 
+// selectDifferentDrivers() — This function takes two arguments, an array of the names of Scuber's drivers and either the returnFirstTwoDrivers() or returnLastTwoDrivers() function. Based on these two arguments, selectDifferentDrivers() will return either the first two drivers or the last two drivers.
 
-const returnLastTwoDrivers = function(drivers) {
-    return drivers.slice(-2)
-}
+const returnFirstTwoDrivers = drivers => drivers.slice(0, 2);
 
+const returnLastTwoDrivers = drivers => drivers.slice(-2);
 
-const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
 
-const createFareMultiplier = number => {
+const createFareMultiplier = integerToMultiply => {
     return function(fare) {
-        return fare * number
+        return fare * integerToMultiply;
     }
 }
 
-const fareDoubler = createFareMultiplier(2)
-const fareTripler = createFareMultiplier(3)
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
 
-// selectDifferentDrivers() — This function takes two arguments, an array of the names of Scuber's drivers and either the returnFirstTwoDrivers() or returnLastTwoDrivers() function. Based on these two arguments, selectDifferentDrivers() will return either the first two drivers or the last two drivers.
-
-const selectDifferentDrivers = function(drivers, driverGroup) {
-    return driverGroup(drivers)
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const returnFirstTwoDrivers = function (drivers) {
-//     return drivers.slice(0, 2)
-// }
-
-// const returnLastTwoDrivers = function (drivers) {
-//     return drivers.slice(-2)
-// }
-
-// const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
-
-// const createFareMultiplier = function (multiplier) {
-//     return function (fare) {
-//         return multiplier * fare
-//     }
-// }
-
-// const fareDoubler = createFareMultiplier(2)
-
-// const fareTripler = createFareMultiplier(3)
-
-// const selectDifferentDrivers = function (drivers, driverGroup) {
-//     return driverGroup(drivers)
-// }
+const selectDifferentDrivers = function(drivers, driverSelection) {
+    return driverSelection(drivers)    
+};
